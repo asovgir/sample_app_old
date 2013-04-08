aa1c44c6405e39929348ab2135371ab7f18ca6b3
Andre Sovgir <andre.sovgir@gmail.com>
2013-04-08 12:06:55 -0700

    Finish user edit, update index, and destoy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,9 +6,9 @@ module SessionsHelper
 	end
 
 	def sign_out
-    self.current_user = nil
-    cookies.delete(:remember_token)
-  end
+	    self.current_user = nil
+	    cookies.delete(:remember_token)
+	  end
 
 	def signed_in?
 		!current_user.nil?
@@ -20,5 +20,18 @@ module SessionsHelper
 
 	def current_user
 		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	end
+
+	def current_user?(user)
+		user == current_user
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	def store_location
+		session[:return_to] = request.url
 	end
 end
